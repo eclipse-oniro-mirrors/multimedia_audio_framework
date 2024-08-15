2df11b7f02de5e227c4ee06b5dbed9414bfcef16
guoyao <guoyao18@huawei.com>
2024-08-15 19:39:53 +0800

    高清无法设备切换问题修复
    Signed-off-by: guoyao <guoyao18@huawei.com>

--- services/audio_service/client/src/renderer_in_client.cpp
+++ services/audio_service/client/src/renderer_in_client.cpp
@@ -318,7 +318,7 @@ void RendererInClientInner::InitDirectPipeType()
         AudioPipeType originType = rendererInfo_.pipeType;
         int32_t type = ipcStream_->GetStreamManagerType();
         if (type == AUDIO_DIRECT_MANAGER_TYPE) {
-            rendererInfo_.pipeType = (rendererInfo_.rendererFlags == AUDIO_FLAG_VOIP_DIRECT) ? 
+            rendererInfo_.pipeType = (rendererInfo_.rendererFlags == AUDIO_FLAG_VOIP_DIRECT) ?
                 PIPE_TYPE_DIRECT_VOIP : PIPE_TYPE_DIRECT_MUSIC;
         } else if (originType == PIPE_TYPE_DIRECT_MUSIC) {
             rendererInfo_.pipeType = PIPE_TYPE_NORMAL_OUT;
