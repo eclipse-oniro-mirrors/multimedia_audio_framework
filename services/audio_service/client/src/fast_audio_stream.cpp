0c7419c18797299677b258bd7256f59873232677
heyabinghyb <heyabing@h-partners.com>
2024-08-26 09:53:11 +0800

    processClient_加判空
    Signed-off-by: heyabinghyb <heyabing@h-partners.com>

--- services/audio_service/client/src/fast_audio_stream.cpp
+++ services/audio_service/client/src/fast_audio_stream.cpp
@@ -370,11 +370,13 @@ int32_t FastAudioStream::Enqueue(const BufferDesc &bufDesc)
 
 void FastAudioStream::SetPreferredFrameSize(int32_t frameSize)
 {
+    CHECK_AND_RETURN_RET_LOG(processClient_, "spkClient is null.");
     processClient_->SetPreferredFrameSize(frameSize);
 }
 
 void FastAudioStream::UpdateLatencyTimestamp(std::string &timestamp, bool isRenderer)
 {
+    CHECK_AND_RETURN_RET_LOG(processClient_, "spkClient is null.");
     processClient_->UpdateLatencyTimestamp(timestamp, isRenderer);
 }
 
